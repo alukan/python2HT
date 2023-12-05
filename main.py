867ca4206c6901c0e585eba020e51bb481281d74
alukan <kolyaterenya@gmail.com>
2023-12-05 15:20:47 +0300

    created

---
+++ main.py
@@ -0,0 +1,12 @@
+class Alien:
+    health = 3
+    alien_counter = 0
+
+    def __init__(self, x_cor=0, y_cor=0):
+        self.x_coor = x_cor
+        self.y_coor = y_cor
+        Alien.alien_counter += 1
+
+
+
+alien = Alien(2, 0)
