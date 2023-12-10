de060c293312f142c88b442384669cc18398912f
alukan <kolyaterenya@gmail.com>
2023-12-10 18:38:37 +0300

    moved to '/', static is not reqiered

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask, render_template, request, send_file
 from google.cloud import translate_v2, texttospeech
-import time
 import os
+import time
 
+app = Flask(__name__)
+
+if not os.path.exists('static'):
+    os.makedirs('static')
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './key.json'
+
+translate_client = translate_v2.Client()
 tts_client = texttospeech.TextToSpeechClient()
 
 SUPPORTED_GENDER_LANGUAGES = {
@@ -13,38 +21,26 @@ SUPPORTED_GENDER_LANGUAGES = {
     'fr': None,  # French
 }
 
-app = Flask(__name__)
-
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './key.json'
-
-translate_client = translate_v2.Client()
-
-
 @app.route('/')
 def home():
-    return 'Hello, welcome to my Flask app!'
+    return render_template('form.html', translated_text=None, audio_file=None)
 
-
-@app.route('/form', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def process_form():
     translated_text = None
-    audio_file = 'static/output.wav'  # Fixed filename
+    audio_file = 'static/output.wav'
 
     user_input = request.form.get('user_input')
     target_language = request.form.get('target_language')
 
-    # Check if target_language is None or empty
     if target_language is None or not target_language.strip():
         return render_template('form.html', error="Target language is required", translated_text=translated_text, audio_file=audio_file)
 
-    # Translate the input to the selected language
     translation = translate_client.translate(user_input, target_language=target_language)
     translated_text = translation['translatedText']
 
-    # Determine the voice gender based on language
     supported_gender = SUPPORTED_GENDER_LANGUAGES.get(target_language, texttospeech.SsmlVoiceGender.NEUTRAL)
 
-    # Generate speech from translated text
     synthesis_input = texttospeech.SynthesisInput(text=translated_text)
     voice_params = texttospeech.VoiceSelectionParams(
         language_code=target_language, ssml_gender=supported_gender
